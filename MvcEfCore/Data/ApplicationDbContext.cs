134bb7525811434aee50f403046430936e67ab4a
Mohammed Fananah <Hp@HP>
2023-02-20 10:47:34 +0200

    Add project files.

---
+++ MvcEfCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcEfCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcEfCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Team> teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ MvcEfCore/Data/Migrations/20220823075456_add-upload-db.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcEfCore.Data.Migrations
+{
+    public partial class adduploaddb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "teams",
+                columns: table => new
+                {
+                    TeamName = table.Column<string>(maxLength: 30, nullable: false),
+                    City = table.Column<string>(nullable: true),
+                    Province = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teams", x => x.TeamName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    TeamName = table.Column<string>(nullable: true),
+                    TeamName1 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerId);
+                    table.ForeignKey(
+                        name: "FK_Players_teams_TeamName1",
+                        column: x => x.TeamName1,
+                        principalTable: "teams",
+                        principalColumn: "TeamName",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamName1",
+                table: "Players",
+                column: "TeamName1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "teams");
+        }
+    }
+}
